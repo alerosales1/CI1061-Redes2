4e986a1e2aed6803b80a1723c7ce7b946496a4e7
Alejandro Gemin Rosales <39320793+alerosales1@users.noreply.github.com>
2021-08-04 12:48:10 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,54 @@
+import codecs
+import json
+
+import requests
+from des import DesKey
+from diffie_hellman import diffie_hellman_des
+import pickle
+
+server_url = 'http://localhost:31364/match'
+
+x = requests.post(server_url)
+
+print(x.text)
+print(x.status_code)
+
+json_msg = bytes(x.text, 'utf-8')
+json_obj = json.loads(json_msg)
+
+diffie_hellman_des_obj = diffie_hellman_des(json_obj['p'], json_obj['g'])
+
+random_num = diffie_hellman_des_obj.generate_random_natural_number(4000, 8000)
+
+local_mix = diffie_hellman_des_obj.calculate_primary_mix(random_num)
+
+answer = {"client_mix":local_mix}
+
+private_key = diffie_hellman_des_obj.calculate_private_key(
+
+                json_obj['server_mix'],
+                random_num
+
+                                       )
+
+print("Private key on client is " + private_key)
+
+private_key_obj = DesKey(bytes(private_key, "utf-8"))
+
+x = requests.post(server_url, json = answer)
+
+user_input = ""
+
+while user_input != "exit":
+
+    user_input = input("Message to server: ")
+
+    rest = len(user_input) % 8
+    if rest != 0:
+        user_input = user_input + " " * (8 - rest)
+
+    crypted_input = private_key_obj.encrypt(bytes(user_input, 'utf-8'))
+
+    answer = {"msg": crypted_input.hex()}
+
+    x = requests.post(server_url, json = answer)
---
+++ diffie_hellman.py
@@ -0,0 +1,27 @@
+import random
+
+class diffie_hellman_des:
+
+    def __init__(self, p, g):
+
+        self.p = p
+        self.g = g
+
+    def generate_random_natural_number(self, min_value, max_value):
+
+        return random.randint(min_value, max_value)
+
+    def calculate_primary_mix(self, random_num):
+
+        result = pow(self.g, random_num) % self.p
+
+        return result
+
+    def calculate_private_key(self, external_mix, local_random_num):
+
+        result = str(pow(external_mix, local_random_num) % self.p)
+
+        if len(result) % 8 != 0:
+            result = result + "0" * (8 - (len(result) % 8))
+
+        return result
