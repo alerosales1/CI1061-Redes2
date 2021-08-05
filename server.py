db0d6bed8ae35fa357919d91264a5b932b571c5c
Alejandro Gemin Rosales <39320793+alerosales1@users.noreply.github.com>
2021-08-05 15:21:06 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #--------------------------------------------------------------------------------------
 #  Programa que implementa o Server
-#  Autor: Alejandro Gemin Rosales e Lucas Santos
+#  Autores: Alejandro Gemin Rosales e Lucas Santos
 #  Disciplina: Redes II
 #  Data da ultima atualizacao: 05/08/21
 #--------------------------------------------------------------------------------------
